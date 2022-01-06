bfb47550ea291c1ae421524788e0ae4a4c2c364c
Sunnan Islam Rahi <sirahi97@gmail.com>
2022-01-06 20:01:26 +0500

    improvements in title card text

--- index.js
+++ index.js
@@ -3,30 +3,50 @@ import {Text} from 'troika-three-text'
 import metaversefile from 'metaversefile';
 const {useApp, usePostOrthographicScene, useFrame, useCleanup, useInternals} = metaversefile;
 
-const materialTitle = new THREE.ShaderMaterial({
-    uniforms:{
-        color:{
-            type: 'v3',
-            value: null
-        },
-        time:{
-            type: 'f',
-            value: 0.0,
-        },
-        startTime:{
-            type: 'f',
-            value: 0.0,
-        },
-        timeFactor: {
-            value: 4.0,
-            needsUpdate: true,
-        },
-        animTime: {
-            value: 6.0,
-            needsUpdate: true,
-        },
+const uniforms = {
+    color:{
+        type: 'v3',
+        value: null
     },
-    
+    time:{
+        type: 'f',
+        value: 0.0,
+    },
+    startTime:{
+        type: 'f',
+        value: 0.0,
+    },
+    timeFactor: {
+        value: 4.0,
+        needsUpdate: true,
+    },
+    animTime: {
+        value: 6.0,
+        needsUpdate: true,
+    },
+    startValue: {
+        value: 0.0,
+        needsUpdate: true,
+    },
+    endValue: {
+        value: 0.0,
+        needsUpdate: true,
+    },
+};
+
+const fragmentShader = `\
+    precision highp float;
+    precision highp int;
+
+    uniform vec3 color;
+    void main()
+    {
+        gl_FragColor = vec4(color, 1.);
+    }
+`;
+
+const materialTitle = new THREE.ShaderMaterial({
+    uniforms,
     vertexShader: `\
         precision highp float;
         precision highp int;
@@ -35,56 +55,28 @@ const materialTitle = new THREE.ShaderMaterial({
         uniform float startTime;
         uniform float timeFactor;
         uniform float animTime;
+        uniform float startValue;
+        uniform float endValue;
         
         const float offsetX = -0.69;
         const float offsetY = 2.65;
         
         void main()
         {
-
-            float f = mod((time - startTime) * timeFactor, animTime);
-            f = min(1.49, f);
-            f *= 2.0;
-            gl_Position = projectionMatrix * vec4(position.x + offsetX, position.y + offsetY - f, -0.1, 1.0);
+            float f = (time - startTime) / ((startTime + animTime/2.0) - startTime);
+            f = clamp(f, 0., 1.);
+            float v = startValue + (endValue - startValue) * f;
+            
+            v = min(1.49, v);
+            v *= 2.0;
+            gl_Position = projectionMatrix * vec4(position.x + offsetX, position.y + offsetY - v, -0.1, 1.0);
         }
-    `
-    ,
-    fragmentShader: `\
-        precision highp float;
-        precision highp int;
-
-        uniform vec3 color;
-        void main()
-        {
-            gl_FragColor = vec4(color, 1.);
-        }
-    `
+    `,
+    fragmentShader
 });
 
 const materialH = new THREE.ShaderMaterial({
-    uniforms:{
-        color:{
-            type: 'v3',
-            value: null
-        },
-        time:{
-            type: 'f',
-            value: 0.0,
-        },
-        startTime:{
-            type: 'f',
-            value: 0.0,
-        },
-        timeFactor: {
-            value: 4.0,
-            needsUpdate: true,
-        },
-        animTime: {
-            value: 6.0,
-            needsUpdate: true,
-        },
-    },
-    
+    uniforms,
     vertexShader: `\
         precision highp float;
         precision highp int;
@@ -93,60 +85,27 @@ const materialH = new THREE.ShaderMaterial({
         uniform float startTime;
         uniform float timeFactor;
         uniform float animTime;
+        uniform float startValue;
+        uniform float endValue;
         
         const float offsetX = 1.01;
         const float offsetY = 0.33;
         
         void main()
         {
-
-            float f = mod((time - startTime) * timeFactor, animTime);
-            if (f > 0.99)
-            {
-                f = min(1.99, f);
-                gl_Position = projectionMatrix * vec4(position.x + offsetX + 0.99 - f, position.y + offsetY, -0.1, 1.0);
-            }
-
+            float f = (time - startTime) / ((startTime + animTime/2.0) - startTime);
+            f = clamp(f, 0., 1.);
+            float v = startValue + (endValue - startValue) * f;
+            
+            v = min(1.99, v);
+            gl_Position = projectionMatrix * vec4(position.x + offsetX + 0.99 - v, position.y + offsetY, -0.1, 1.0);
         }
-    `
-    ,
-    fragmentShader: `\
-        precision highp float;
-        precision highp int;
-
-        uniform vec3 color;
-        
-        void main()
-        {
-            gl_FragColor = vec4(color, 1.);
-        }
-    `
+    `,
+    fragmentShader
 });
 
 const materialSh = new THREE.ShaderMaterial({
-    uniforms:{
-        color:{
-            type: 'v3',
-            value: null
-        },
-        time:{
-            type: 'f',
-            value: 0.0,
-        },
-        startTime:{
-            type: 'f',
-            value: 0.0,
-        },
-        timeFactor: {
-            value: 4.0,
-            needsUpdate: true,
-        },
-        animTime: {
-            value: 6.0,
-            needsUpdate: true,
-        },
-    },
-    
+    uniforms,
     vertexShader: `\
         precision highp float;
         precision highp int;
@@ -155,59 +114,27 @@ const materialSh = new THREE.ShaderMaterial({
         uniform float startTime;
         uniform float timeFactor;
         uniform float animTime;
+        uniform float startValue;
+        uniform float endValue;
         
         const float offsetX = 1.01;
         const float offsetY = 0.14;
         
         void main()
         {
-            float f = mod((time - startTime) * timeFactor, animTime);
-            if (f > 1.99)
-            {
-                f = min(2.99, f);
-                gl_Position = projectionMatrix * vec4(position.x + offsetX + 1.99 - f, position.y + offsetY, -0.1, 1.0);
-            }
+            float f = (time - startTime) / ((startTime + animTime/2.0) - startTime);
+            f = clamp(f, 0., 1.);
+            float v = startValue + (endValue - startValue) * f;
+            
+            v = min(2.99, v);
+            gl_Position = projectionMatrix * vec4(position.x + offsetX + 1.99 - v, position.y + offsetY, -0.1, 1.0);
         }
-    `
-    ,
-    fragmentShader: `\
-        precision highp float;
-        precision highp int;
-        
-        uniform vec3 color;
-        
-        void main()
-        {
-            gl_FragColor = vec4(color, 1.);
-        }
-    `
-    ,
+    `,
+    fragmentShader
 });
 
 const materialText = new THREE.ShaderMaterial({
-    uniforms:{
-        color:{
-            type: 'v3',
-            value: null
-        },
-        time:{
-            type: 'f',
-            value: 0.0,
-        },
-        startTime:{
-            type: 'f',
-            value: 0.0,
-        },
-        timeFactor: {
-            value: 4.0,
-            needsUpdate: true,
-        },
-        animTime: {
-            value: 6.0,
-            needsUpdate: true,
-        },
-    },
-    
+    uniforms,
     vertexShader: `\
         precision highp float;
         precision highp int;
@@ -216,32 +143,23 @@ const materialText = new THREE.ShaderMaterial({
         uniform float startTime;
         uniform float timeFactor;
         uniform float animTime;
+        uniform float startValue;
+        uniform float endValue;
         
         const float offsetX = 1.01;
         const float offsetY = 0.0;
         
         void main()
         {
-
-            float f = mod((time - startTime) * timeFactor, animTime);
-            if (f > 2.99)
-            {
-                f = min(3.99, f);
-                gl_Position = projectionMatrix * vec4(position.x + offsetX + 2.99 - f, position.y + offsetY, -0.1, 1.0);
-            }
+            float f = (time - startTime) / ((startTime + animTime/2.0) - startTime);
+            f = clamp(f, 0., 1.);
+            float v = startValue + (endValue - startValue) * f;
+            
+            v = min(3.99, v);
+            gl_Position = projectionMatrix * vec4(position.x + offsetX + 2.99 - v, position.y + offsetY, -0.1, 1.0);
         }
-    `
-    ,
-    fragmentShader: `\
-        precision highp float;
-        precision highp int;
-
-        uniform vec3 color;
-        void main()
-        {
-            gl_FragColor = vec4(color, 1.);
-        }
-    `
+    `,
+    fragmentShader
 });
 
 let _update = null;
@@ -253,73 +171,80 @@ let text = null;
 
 let objs = [null, null, null, null];
 
+async function makeTextMesh(
+    text = '',
+    material = null,
+    font = './fonts/Plaza Regular.ttf',
+    fontSize = 1,
+    letterSpacing = 0,
+    anchorX = 'left',
+    anchorY = 'middle',
+    color = 0x000000,
+  ) {
+    const textMesh = new Text();
+    textMesh.text = text;
+    if (material !== null) {
+      textMesh.material = material;
+    }
+    textMesh.font = font;
+    textMesh.fontSize = fontSize;
+    textMesh.letterSpacing = letterSpacing;
+    textMesh.color = color;
+    textMesh.anchorX = anchorX;
+    textMesh.anchorY = anchorY;
+    textMesh.frustumCulled = false;
+    await new Promise(accept => {
+      textMesh.sync(accept);
+    });
+    return textMesh;
+  }
+
 export default e => {
     const app = useApp();
     const postSceneOrthographic = usePostOrthographicScene();
 
     e.waitUntil((async()=>{
-        {   
-            title = new Text();
-            title.text = "WEBAVERSE";
-            title.font = './fonts/Plaza Regular.ttf';
-            title.fontSize = 0.05;
-            title.anchorX = 'middle';
-            title.anchorY = 'middle';
-            title.material = materialTitle;
-            
-            title.material.uniforms.color.value = new THREE.Vector3(1.0, 1.0, 1.0);
-
+        {
+            title = await makeTextMesh(
+                'WEBAVERSE',
+                materialTitle,
+                undefined,
+                0.05
+            )
             objs[0] = title;
-                        
-            title.sync();
             app.add(title);
         }
 
         {
-            heading = new Text();
-            heading.font = './fonts/Plaza Regular.ttf';
-            heading.fontSize = 0.13;
-            heading.anchorX = 'left';
-            heading.anchorY = 'middle';
-            heading.material = materialH;
-            
-            heading.material.uniforms.color.value = new THREE.Vector3(1.0, 1.0, 1.0);
-                        
+            heading = await makeTextMesh(
+                '',
+                materialH,
+                undefined,
+                0.13
+            )
             objs[1] = heading;
-
-            heading.sync();
             app.add(heading);
         }
 
         {
-            subHeading = new Text();
-            subHeading.font = './fonts/Plaza Regular.ttf';
-            subHeading.fontSize = 0.07;
-            subHeading.anchorX = 'left';
-            subHeading.anchorY = 'middle';
-            subHeading.material = materialSh;
-            
-            subHeading.material.uniforms.color.value = new THREE.Vector3(1.0, 1.0, 1.0);
-            
-            objs[2] = subHeading;           
-            
-            subHeading.sync();
+            subHeading = await makeTextMesh(
+                '',
+                materialSh,
+                undefined,
+                0.07
+            )
+            objs[2] = subHeading;
             app.add(subHeading);
         }
 
         {
-            text = new Text();
-            text.font = './fonts/Plaza Regular.ttf';
-            text.fontSize = 0.08;
-            text.anchorX = 'left';
-            text.anchorY = 'middle';
-            text.material = materialText;
-            
-            text.material.uniforms.color.value = new THREE.Vector3(1.0, 1.0, 1.0);
-            
-            objs[3] = text;           
-            
-            text.sync();
+            text = await makeTextMesh(
+                '',
+                materialText,
+                undefined,
+                0.08
+            )
+            objs[3] = text;
             app.add(text);
         }
     })());
